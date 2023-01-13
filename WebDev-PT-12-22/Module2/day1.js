37bab80bb11ca2f9e7d11beaf547b437719b489c
Software Kengineer <117790637+SoftwareKengineer@users.noreply.github.com>
2023-01-13 02:38:27 -0500

    Add files via upload
    deleted first webdev to try it again

---
+++ WebDev-PT-12-22/Module2/day1.js
@@ -0,0 +1,53 @@
+// This is a single line comment : This will be ignored by JavaScript, i.e. it will not be executed 
+// Create 4 string variables : a firstName, a lastName, currentDate (ex: 10/28/2021 date would be 28), and programming language
+
+console.log = firstName: 'Software',
+lastName: 'Kengineer',
+currentDate: '12/20/2022',
+programLanguage: 'JavaScript'
+}
+
+// Create a function that introduces yourself: It should take 2 parameters: a first name and a last name
+function whatsmyname(){
+console.log(`my name is ${myInfo.firstName} ${myInfo.lastName}`)
+const currentMonth= 'December'
+const currentYear= 2022
+
+
+}
+
+whatsmyname()
+// inside the function block create 2 variables; 1 string and another number: currentMonth and currentYear
+
+
+// let's introduce ourself with console.log and template literals; i.e. `I am an example of a template literal`
+
+
+// ex. Hello! My name is firstName lastName < pass in your parameters
+
+
+// ex. The date is the currentDate of currentMonth, currentYear
+
+
+// ex. I am currently learning how to program in programming language
+
+
+// ex. Change the value of the programming language variable
+
+
+// Add a console.log to output programming language 
+
+
+// ex. And I'm learning language
+
+
+// ex. I now know how to make a variables and functions
+
+
+// exit out of the function
+
+
+// console.log a global scoped variable; i.e. a variable not defined in a code block
+
+
+// console.log a local scoped variable defined in the function block
---
+++ WebDev-PT-12-22/Module3/day3cat.md
@@ -0,0 +1,4 @@
+<div class="container">
+  <img src="cat.jpeg" alt="cat" />
+  <span>The internet loves cats</span>
+</div>
